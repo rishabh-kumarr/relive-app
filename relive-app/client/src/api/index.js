d7f4c62122f98ddf106926614f87970db2b913c9
Rishabh Kumar <thisisrishabhkumar@gmail.com>
2021-04-10 22:17:47 +0530

    Final Commit

--- relive-app/client/src/api/index.js
+++ relive-app/client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const API = axios.create({ baseURL: 'https://relive-app.herokuapp.com' })
-const API = axios.create({ baseURL: 'http://localhost:5000' })
+const API = axios.create({ baseURL: 'https://relive-socialmedia.herokuapp.com' })
 
 API.interceptors.request.use((req) => {
 	if (localStorage.getItem('profile')) {
